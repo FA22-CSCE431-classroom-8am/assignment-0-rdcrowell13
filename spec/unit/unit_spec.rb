fe1d83f26d29dc04e1cad5b55c1ebcdcef98fe2a
rdcrowell13 <rdcrowell13@gmail.com>
2022-09-15 23:18:22 -0500

    Added attributes. Fixed unit and integration tests

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'JK Rowling', price: '9.99', published: '2022-09-15')
   end
 
   it 'is valid with valid attributes' do
@@ -14,36 +14,20 @@ RSpec.describe Book, type: :model do
     subject.title = nil
     expect(subject).not_to be_valid
   end
-end
 
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(author: 'J.K. Rowling')
-  end
-  
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-end
-
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(price: 10)
-  end
-  
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-end
-
-
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(published_date: DateTime.now)
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
   end
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a date' do
+    subject.published = nil
+    expect(subject).not_to be_valid
   end
 end
 
